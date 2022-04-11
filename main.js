cf5251d32b860379e1cd5f67a83747d8c94d850c
Spencer Meyer <spencer.meyer@go.mnstate.edu>
2022-04-10 23:22:35 -0500

    third commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
 function selectCurrency() {  
+
     var currencyList = document.getElementById("currencyList");
     var currencyType = currencyList.options[currencyList.selectedIndex]
     getAmount(currencyType); 
@@ -40,96 +40,4 @@ function getAmount(selectedObject) {
         document.getElementById("amount").value = "N/A";
     }
 
-    
 }
-
-/*
-function getAmount(selectedObject) {
-    var element = selectedObject.value;
-    if (element == "USD"){
-        fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-            .then(res => res.json())
-            .then(data => {
-                var amount = `${data.bpi.USD.rate}`;
-                document.getElementById("amount").value = amount;
-            })
-    }
-
-    else if(element == "GBP"){
-        fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-            .then(res => res.json())
-            .then(data => {
-                var amount = `${data.bpi.GBP.rate}`;
-                document.getElementById("amount").value = amount;
-            })
-    }
-
-    else if(element == "EUR"){
-        fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-            .then(res => res.json())
-            .then(data => {
-                var amount = `${data.bpi.EUR.rate}`;
-                document.getElementById("amount").value = amount;
-            })
-    }
-
-    else {
-        document.getElementById("amount").value = "N/A";
-    }
-
-    
-}
-
-*/
-
-
-
-
-
-
-
-
-//test_btn.addEventListener("click", getTestResult)
-//const result = document.getElementById("result");
-/*
-function getTestResult() {
-
-    
-        .then(res => res.json())
-        .then(data => {
-            result.innerHTML += `<p>${data.time.updated}"</p>`
-        })
-
-}
-
-
-
-function fetchFacts() {
-
-    let exampleText = "hello";
-    console.log(exampleText);
-
-}
-
-function fetch2() {
-
-    fetch("https://https://catfact.ninja/fact?max_length=50'")
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => console.log(err))
-
-}
-
-fetch("https://https://catfact.ninja/fact")
-.then(response => response.json())
-.then(data => console.log(data))
-
-function fetch3() {
-    
-    fetch("https://https://catfact.ninja/fact")
-    .then(response => response.json())
-    .then(data => console.log(data))
-}
-*/
